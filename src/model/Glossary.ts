42778dda1f31dc68dcece65f9f9fadd4a0dab9be
Le Quang Thang <lethang7794@gmail.com>
2024-08-21 14:46:29 +0700

    add glossary service

---
+++ src/model/Glossary.ts
@@ -0,0 +1,5 @@
+export type Glossary = {
+  docs: string
+  term: string
+  explain: string
+}
---
+++ src/service/glossary.ts
@@ -0,0 +1,18 @@
+import { cache } from 'react'
+import fs from 'node:fs'
+import yaml from 'yaml'
+import type { Glossary } from '@/model/Glossary'
+
+const GLOSSARY_REPO_PATH = 'data/glossary-QCVN.yaml'
+const GLOSSARY_PUBLIC_PATH = 'glossary'
+
+export const getGlossaries = cache(() => {
+  const file = fs.readFileSync(GLOSSARY_REPO_PATH).toString()
+  const data = yaml.parse(file)
+  return data as Record<string, Glossary>
+})
+
+export function getGlossaryById(id: string): Glossary | undefined {
+  const items = getGlossaries()
+  return items[id]
+}
