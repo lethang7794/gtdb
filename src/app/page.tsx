9e6d9e9f20f611d231b8440ff251d9c490b60c6c
Le Quang Thang <lethang7794@gmail.com>
2024-08-24 18:10:08 +0700

    reformat home page list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,28 @@ import Link from 'next/link'
 export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col justify-between p-6 md:p-8">
-      <div className="flex flex-col gap-2">
-        <div>
+      <ol className="flex flex-col gap-2">
+        <li>
           <Link className="text-5xl text-blue-500" href="/bbgt">
             Biển báo giao thông
           </Link>
-        </div>
-        <div>
+        </li>
+        <li>
           <Link className="text-5xl text-blue-500" href="/vach-ke-duong">
             Vạch kẻ đường
           </Link>
-        </div>
-        <div>
+        </li>
+        <li>
           <Link className="text-5xl text-blue-500" href="/thuat-ngu">
             Thuật ngữ
           </Link>
-        </div>
-        <div>
+        </li>
+        <li>
           <Link className="text-5xl text-blue-500" href="/vbpl/luat-GTDB-2008">
             Luật Giao thông đường bộ 2008 (Có hiệu lực đến 31/12/2024)
           </Link>
-        </div>
-        <div>
+        </li>
+        <li>
           <Link
             className="text-5xl text-blue-500"
             href="/vbpl/luat-TTATGTDB-2024"
@@ -32,16 +32,16 @@ export default async function Home() {
             Luật Trật tự, an toàn giao thông đường bộ 2024 (Có hiệu lực từ
             1/1/2025)
           </Link>
-        </div>
-        <div>
+        </li>
+        <li>
           <Link
             className="text-5xl text-blue-500"
             href="/vbpl/toc-do-khoang-cach-an-toan"
           >
             Quy định về tốc độ và khoảng cách an toàn (2023)
           </Link>
-        </div>
-      </div>
+        </li>
+      </ol>
     </div>
   )
 }
