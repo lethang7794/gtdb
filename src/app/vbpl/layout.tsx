c74e280a28145011c42f6dbfeee9f6b27dbf6b9d
Le Quang Thang <lethang7794@gmail.com>
2024-08-24 16:14:20 +0700

    extract GITHUB_MARKDOWN_CSS_CLASS

--- src/app/vbpl/layout.tsx
+++ src/app/vbpl/layout.tsx
@@ -1,9 +1,11 @@
 import 'github-markdown-css/github-markdown.css'
 import './style.css'
 
+const GITHUB_MARKDOWN_CSS_CLASS = 'markdown-body'
+
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="markdown-body">
+    <div className={GITHUB_MARKDOWN_CSS_CLASS}>
       <div className="flex min-h-screen flex-col justify-between p-6 md:p-8">
         <div className="container">{children}</div>
       </div>
