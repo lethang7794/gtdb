c116da0265943fe0eb16f3064d53ec51d0ca1bc3
Le Quang Thang <lethang7794@gmail.com>
2024-09-04 12:53:38 +0700

    refactor homepage links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,25 @@
 import Link from 'next/link'
+import { LINKS } from '@/constant/homepage-links'
 
 export default async function Home() {
   return (
     <div className="flex h-full flex-col justify-between m-6 md:m-8">
       <ol className="flex flex-col gap-2 list-decimal p-2 ml-8 text-2xl">
-        <li>
-          <Link
-            className="text-blue-600 dark:text-blue-500 hover:underline"
-            href="/bbgt"
-          >
-            Biển báo giao thông
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="text-blue-600 dark:text-blue-500 hover:underline"
-            href="/vach-ke-duong"
-          >
-            Vạch kẻ đường
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="text-blue-600 dark:text-blue-500 hover:underline"
-            href="/thuat-ngu"
-          >
-            Thuật ngữ
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="text-blue-600 dark:text-blue-500 hover:underline mr-2"
-            href="/vbpl/luat-GTDB-2008"
-          >
-            Luật Giao thông đường bộ (2008)
-          </Link>
-          <br />
-          <span>(Có hiệu lực đến 31/12/2024)</span>
-        </li>
-        <li>
-          <Link
-            className="text-blue-600 dark:text-blue-500 hover:underline mr-2"
-            href="/vbpl/luat-TTATGTDB-2024"
-          >
-            Luật Trật tự, an toàn giao thông đường bộ (2024)
-          </Link>
-          <br />
-          <span>(Có hiệu lực từ 1/1/2025)</span>
-        </li>
-        <li>
-          <Link
-            className="text-blue-600 dark:text-blue-500 hover:underline"
-            href="/vbpl/toc-do-khoang-cach-an-toan"
-          >
-            Quy định về tốc độ và khoảng cách an toàn (2023)
-          </Link>
-        </li>
+        {LINKS.map((item) => {
+          return (
+            <li key={item.name + item.url}>
+              <Link
+                className="text-blue-600 dark:text-blue-500 hover:underline mr-2"
+                href={item.url}
+              >
+                {item.name}
+              </Link>
+              {item.description ? (
+                <div className="text-gray-500 italic">({item.description})</div>
+              ) : null}
+            </li>
+          )
+        })}
       </ol>
     </div>
   )
---
+++ src/constant/homepage-links.tsx
@@ -0,0 +1,27 @@
+export const LINKS: {
+  name: string
+  url: string
+  description?: string
+}[] = [
+  { name: 'Biển báo giao thông', url: '/bbgt', description: '' },
+  { name: 'Vạch kẻ đường', url: '/vach-ke-duong', description: '' },
+  { name: 'Thuật ngữ', url: '/thuat-ngu', description: '' },
+  {
+    name: 'Luật Giao thông đường bộ (2008)',
+
+    url: '/vbpl/luat-GTDB-2008',
+    description: 'Có hiệu lực đến 31/12/2024',
+  },
+  {
+    name: 'Luật Trật tự, an toàn giao thông đường bộ (2024)',
+
+    url: '/vbpl/luat-TTATGTDB-2024',
+    description: 'Có hiệu lực từ 1/1/2025',
+  },
+  {
+    name: 'Quy định về tốc độ và khoảng cách an toàn (2023)',
+
+    url: '/vbpl/toc-do-khoang-cach-an-toan',
+    description: '',
+  },
+]
