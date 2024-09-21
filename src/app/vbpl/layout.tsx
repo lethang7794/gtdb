8bfaabe40367ff85a21ab910cd65f3ae735d8b95
Le Quang Thang <lethang7794@gmail.com>
2024-09-21 15:19:54 +0700

    reformat: vbpl

--- src/app/vbpl/layout.tsx
+++ src/app/vbpl/layout.tsx
@@ -6,7 +6,7 @@ import { GITHUB_MARKDOWN_CSS_CLASS } from '@/constant/github-markdown-css'
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className={GITHUB_MARKDOWN_CSS_CLASS}>
-      <div className="flex h-full flex-col justify-between p-6 md:p-8">
+      <div className="flex h-full flex-col justify-between py-6 md:py-8">
         <div className="container">{children}</div>
       </div>
     </div>
