cf54875068a126e3b0aea6873a415bb612a72642
Le Quang Thang <lethang7794@gmail.com>
2024-09-21 15:06:15 +0700

    fix: mdx toc style

--- src/app/vbpl/layout.tsx
+++ src/app/vbpl/layout.tsx
@@ -1,5 +1,6 @@
 import 'github-markdown-css/github-markdown.css'
 import '@/style/github-markdown.css'
+import '@/style/github-markdown-custom.css'
 import { GITHUB_MARKDOWN_CSS_CLASS } from '@/constant/github-markdown-css'
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
