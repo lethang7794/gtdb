0a3f93955d2b05c45682a4b231e1ddc0c956a0fc
Le Quang Thang <lethang7794@gmail.com>
2024-10-15 00:44:05 +0700

    data: extra links

--- src/constant/homepage-links.tsx
+++ src/constant/homepage-links.tsx
@@ -35,3 +35,103 @@ export const LINKS: {
     description: '',
   },
 ]
+
+export const EXTRA_LINKS: {
+  name: string
+  url: string
+  description?: string
+}[] = [
+  {
+    name: 'Dashboard | Cục đường bộ Việt Nam',
+    url: 'https://ttdh.drvn.gov.vn/#/dashboard',
+    description: '',
+  },
+  { name: 'Bản đồ cầu đường bộ', url: 'http://gis.drvn.vn/', description: '' },
+  {
+    name: 'Trang thông tin Giấy phép lái xe | Cục đường bộ Việt Nam',
+    url: 'https://gplx.gov.vn/',
+    description:
+      'TRA CỨU THÔNG TIN Giấy phép lái xe: GPLX PET (có thời hạn, không thời hạn), GPLX cũ',
+  },
+  {
+    name: 'Các Bản Đồ Đường Bộ Chuyên Biệt | Cục đường bộ Việt Nam',
+    url: 'https://sites.google.com/view/ttcddh-nv/b%E1%BA%A3n-%C4%91%E1%BB%93-s%E1%BB%91',
+    description: '',
+  },
+  {
+    name: 'Mạng lưới đường cao tốc và quốc lộ do các Khu quản lý',
+    url: 'https://www.google.com/maps/d/viewer?mid=1Rt3-7UEr_p2ALodP9opRKLPeQv4_mjA',
+    description: '',
+  },
+  {
+    name: 'Mạng lưới đường cao tốc và tiền cao tốc tính đến 4/2023',
+    url: 'https://www.google.com/maps/d/viewer?mid=1ndNjcUwnD7JjmCoOmB8sx6hvPTI5G_Q',
+    description: '',
+  },
+  {
+    name: 'Các tuyến quốc lộ do các Khu quản lý',
+    url: 'https://www.google.com/maps/d/viewer?mid=1TzOYTUnUwL5D9BtbaGVLFLR_7YAK1xg',
+    description: '',
+  },
+  {
+    name: 'Mạng lưới đường bộ quốc tế ở Việt Nam',
+    url: 'https://www.google.com/maps/d/viewer?mid=1Y8Kctl-7IvmSRbvGSyHq76BrPe04cx4',
+    description: '',
+  },
+  {
+    name: 'Bản đồ quy hoạch hệ thống giao thông Việt Nam đến năm 2050',
+    url: 'https://drive.google.com/file/d/1r3wA8PrcDyJ_H0I_dU5jm7x5jVXq4Its/view',
+    description: '',
+  },
+  {
+    name: '[Bản nháp] So sánh hệ thống biển báo giao thông các nước châu Á',
+    url: 'https://en.wikipedia.org/wiki/Draft:Comparison_of_Asian_road_signs',
+    description: '',
+  },
+  {
+    name: 'So sánh hệ thống biển báo giao thông các nước ảnh hưởng bởi MUTCD',
+    url: 'https://en.wikipedia.org/wiki/Comparison_of_MUTCD-influenced_traffic_signs#',
+    description:
+      'Manual on Uniform Traffic Control Devices MUTCD (https://mutcd.fhwa.dot.gov/)',
+  },
+  {
+    name: 'So sánh hệ thống biển báo giao thông các nước Châu Âu',
+    url: 'https://en.wikipedia.org/wiki/Comparison_of_European_road_signs',
+    description: '',
+  },
+  // {
+  //   name: '',
+  //   url: '',
+  //   description: '',
+  // },
+  // {
+  //   name: '',
+  //   url: '',
+  //   description: '',
+  // },
+  // {
+  //   name: '',
+  //   url: '',
+  //   description: '',
+  // },
+  // {
+  //   name: '',
+  //   url: '',
+  //   description: '',
+  // },
+  // {
+  //   name: '',
+  //   url: '',
+  //   description: '',
+  // },
+  // {
+  //   name: '',
+  //   url: '',
+  //   description: '',
+  // },
+  // {
+  //   name: '',
+  //   url: '',
+  //   description: '',
+  // },
+]
