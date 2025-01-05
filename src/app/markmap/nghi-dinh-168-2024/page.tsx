051c42dbdb62f15c7c942943d805b315685866e5
Le Quang Thang <lethang7794@gmail.com>
2025-01-05 18:35:00 +0700

    Show mind map in nghi dinh 168

--- src/app/markmap/nghi-dinh-168-2024/page.tsx
+++ src/app/markmap/nghi-dinh-168-2024/page.tsx
@@ -2,7 +2,7 @@ import { getNghiDinh168MindMap } from '@/service/nghi-dinh-168-mind-map'
 import Markmap from '@/components/markmap'
 import './style.css'
 
-export default async function BietTuongListPage() {
+export default async function NghiDinh168Mindmap() {
   const data = getNghiDinh168MindMap()
 
   return <Markmap data={data} />
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -3,12 +3,14 @@ import type { MDXComponents } from 'mdx/types'
 import { DocumentHeading } from '@/components/mdx/document-heading'
 import { DocumentEnding } from '@/components/mdx/document-ending'
 import { CollapsibleSection } from '@/components/block/collapsible'
+import NghiDinh168Mindmap from '@/app/markmap/nghi-dinh-168-2024/page'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     DocumentHeading,
     DocumentEnding,
     CollapsibleSection,
+    NghiDinh168Mindmap,
     ...components,
   }
 }
