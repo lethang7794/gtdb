42a33f947eb876ab4304cd384e5d0068ce53e942
Le Quang Thang <lethang7794@gmail.com>
2025-01-06 00:35:10 +0700

    fix markmap style

--- src/app/markmap/nghi-dinh-168-2024/page.tsx
+++ src/app/markmap/nghi-dinh-168-2024/page.tsx
@@ -1,6 +1,5 @@
 import { getNghiDinh168MindMap } from '@/service/nghi-dinh-168-mind-map'
 import Markmap from '@/components/markmap'
-import './style.css'
 
 export default async function NghiDinh168Mindmap() {
   const data = getNghiDinh168MindMap()
--- src/app/markmap/phuong-tien-tham-gia-GTDB/page.tsx
+++ src/app/markmap/phuong-tien-tham-gia-GTDB/page.tsx
@@ -1,7 +1,6 @@
 import { getVehicles } from '@/service/vehicle'
 import Markmap from '@/components/markmap'
 import ChuThich from './ChuThich'
-import './style.css'
 
 export default async function BietTuongListPage() {
   const data = getVehicles()
