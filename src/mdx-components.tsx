d396f6d5cff4c19f13fb80302d2ad7015557dbbb
Le Quang Thang <lethang7794@gmail.com>
2025-01-07 01:58:09 +0700

    hackaround to edit TOC href

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -3,8 +3,10 @@ import type { MDXComponents } from 'mdx/types'
 import { DocumentHeading } from '@/components/mdx/document-heading'
 import { DocumentEnding } from '@/components/mdx/document-ending'
 import { CollapsibleSection } from '@/components/block/collapsible'
+
 import NghiDinh168Mindmap from '@/app/markmap/nghi-dinh-168-2024/page'
 import A from '@/components/block/anchor-link'
+import Cleanup168 from '@/app/vbpl/nghi-dinh-168-2024/cleanup'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -13,6 +15,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     CollapsibleSection,
     NghiDinh168Mindmap,
     A,
+    Cleanup168,
     ...components,
   }
 }
