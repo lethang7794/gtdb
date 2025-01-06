d3ed8a1b1b02be71d4207bbc81c63b9df94847e7
Le Quang Thang <lethang7794@gmail.com>
2025-01-06 16:57:39 +0700

    add metadata for nghi dinh 168 pages

--- src/app/markmap/nghi-dinh-168-2024/page.tsx
+++ src/app/markmap/nghi-dinh-168-2024/page.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from 'next'
 import { getNghiDinh168MindMap } from '@/service/nghi-dinh-168-mind-map'
 import Markmap from '@/components/markmap'
+
+export const metadata: Metadata = {
+  title: 'Nghị định 168/2024 - Tóm tắt',
+  description:
+    'Quy định xử phạt vi phạm hành chính về trật tự, an toàn giao thông trong lĩnh vực giao thông đường bộ; trừ điểm phục hồi điểm giấy phép lái xe',
+}
 
 export default async function NghiDinh168Mindmap() {
   const data = getNghiDinh168MindMap()
--- src/app/vbpl/nghi-dinh-168-2024/page.tsx
+++ src/app/vbpl/nghi-dinh-168-2024/page.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from 'next'
 import NghiDinh1682024 from '@/content/168.2024.NĐ.CP.mdx'
 import './style.css'
+
+export const metadata: Metadata = {
+  title: 'Nghị định 168/2024',
+  description:
+    'Quy định xử phạt vi phạm hành chính về trật tự, an toàn giao thông trong lĩnh vực giao thông đường bộ; trừ điểm phục hồi điểm giấy phép lái xe',
+}
 
 export default function NghiDinh1682024Page() {
   return <NghiDinh1682024 />
