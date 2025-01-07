4474a49505ee9573b019961eef6b2066f61a88bc
Le Quang Thang <lethang7794@gmail.com>
2025-01-08 00:52:00 +0700

    fix local assets in production

--- src/service/nghi-dinh-168.ts
+++ src/service/nghi-dinh-168.ts
@@ -1,12 +1,15 @@
 import { cache } from 'react'
 import fs from 'node:fs'
+import { join } from 'node:path'
 import yaml from 'yaml'
 import type { ND168 } from '@/model/ND168'
 
 const MARKINGS_REPO_PATH = 'data/nghi-dinh-168.yaml'
 
 export const getND168s = cache(() => {
-  const file = fs.readFileSync(MARKINGS_REPO_PATH).toString()
+  const file = fs
+    .readFileSync(join(process.cwd(), MARKINGS_REPO_PATH))
+    .toString()
   const data = yaml.parse(file)
   return data as Record<string, ND168>
 })
