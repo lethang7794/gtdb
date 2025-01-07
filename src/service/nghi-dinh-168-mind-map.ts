95ab55ccc30ce4b666d408b635ca55cd578c9090
Le Quang Thang <lethang7794@gmail.com>
2025-01-08 00:56:47 +0700

    fix local assets 168 mind map

--- src/service/nghi-dinh-168-mind-map.ts
+++ src/service/nghi-dinh-168-mind-map.ts
@@ -1,8 +1,9 @@
 import { cache } from 'react'
 import fs from 'node:fs'
+import { join } from 'node:path'
 
 const NGHI_DINH_168_MIND_MAP = 'src/content/nghi-dinh-168-mind-map.md'
 
 export const getNghiDinh168MindMap = cache(() => {
-  return fs.readFileSync(NGHI_DINH_168_MIND_MAP).toString()
+  return fs.readFileSync(join(process.cwd(), NGHI_DINH_168_MIND_MAP)).toString()
 })
