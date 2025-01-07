6bc417e2139ca5d32639499bd6a9c80e13a79077
Le Quang Thang <lethang7794@gmail.com>
2025-01-08 02:57:40 +0700

    remove detail

--- src/lib/explain-share-link.ts
+++ src/lib/explain-share-link.ts
@@ -1,35 +1,23 @@
-import type { ND168 } from '@/model/ND168'
-import { getND168ById } from '@/service/nghi-dinh-168'
-
 export function explain168Section(id?: string):
   | {
       type: 'chuong'
       path: string
-      chuong?: ND168
     }
   | {
       type?: 'muc'
       path: string
-      chuong?: ND168
-      muc?: ND168
     }
   | {
       type: 'dieu'
       path: string
-      dieu?: ND168
     }
   | {
       type: 'khoan'
       path: string
-      dieu?: ND168
-      khoan?: ND168
     }
   | {
       type: 'diem'
       path: string
-      dieu?: ND168
-      khoan?: ND168
-      diem?: ND168
     }
   | {
       path: ''
@@ -38,13 +26,10 @@ export function explain168Section(id?: string):
     return { path: '' }
   }
 
-  const curDetail = getND168ById(id)
-
   if (isChuong(id)) {
     return {
       type: 'chuong',
       path: `Chương ${id}`,
-      chuong: curDetail,
     }
   }
   if (isMuc(id)) {
@@ -52,15 +37,12 @@ export function explain168Section(id?: string):
     return {
       type: 'muc',
       path: `Chương ${chuong}, mục ${muc}`,
-      chuong: getND168ById(chuong),
-      muc: curDetail,
     }
   }
   if (isDieu(id)) {
     return {
       type: 'dieu',
       path: `Điều ${id}`,
-      dieu: curDetail,
     }
   }
   if (isKhoan(id)) {
@@ -68,8 +50,6 @@ export function explain168Section(id?: string):
     return {
       type: 'khoan',
       path: `Khoản ${khoan}, Điều ${dieu}`,
-      dieu: curDetail,
-      khoan: getND168ById(khoan),
     }
   }
   if (isDiem(id)) {
@@ -77,9 +57,6 @@ export function explain168Section(id?: string):
     return {
       type: 'diem',
       path: `Điểm ${diem}, khoản ${khoan}, Điều ${dieu}`,
-      dieu: getND168ById(dieu),
-      khoan: getND168ById(khoan),
-      diem: curDetail,
     }
   }
 
