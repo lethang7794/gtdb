f1c7e20f1286b7978131e6c757ca0c74fc1a0f73
Le Quang Thang <lethang7794@gmail.com>
2025-01-08 02:56:01 +0700

    get detail of section

--- src/lib/explain-share-link.ts
+++ src/lib/explain-share-link.ts
@@ -1,24 +1,107 @@
-export function explain168Section(id?: string): { path: string } {
+import type { ND168 } from '@/model/ND168'
+import { getND168ById } from '@/service/nghi-dinh-168'
+
+export function explain168Section(id?: string):
+  | {
+      type: 'chuong'
+      path: string
+      chuong?: ND168
+    }
+  | {
+      type?: 'muc'
+      path: string
+      chuong?: ND168
+      muc?: ND168
+    }
+  | {
+      type: 'dieu'
+      path: string
+      dieu?: ND168
+    }
+  | {
+      type: 'khoan'
+      path: string
+      dieu?: ND168
+      khoan?: ND168
+    }
+  | {
+      type: 'diem'
+      path: string
+      dieu?: ND168
+      khoan?: ND168
+      diem?: ND168
+    }
+  | {
+      path: ''
+    } {
   if (!id) {
     return { path: '' }
   }
-  if (id.match(/^(I|II|III|IV)$/)) {
-    return { path: `Chương ${id}` }
+
+  const curDetail = getND168ById(id)
+
+  if (isChuong(id)) {
+    return {
+      type: 'chuong',
+      path: `Chương ${id}`,
+      chuong: curDetail,
+    }
   }
-  if (id.match(/^(I|II|III|IV)\.(\d+)$/)) {
+  if (isMuc(id)) {
     const [chuong, muc] = id.split('.')
-    return { path: `Chương ${chuong}, mục ${muc}` }
+    return {
+      type: 'muc',
+      path: `Chương ${chuong}, mục ${muc}`,
+      chuong: getND168ById(chuong),
+      muc: curDetail,
+    }
   }
-  if (id.match(/^\d+$/)) {
-    return { path: `Điều ${id}` }
+  if (isDieu(id)) {
+    return {
+      type: 'dieu',
+      path: `Điều ${id}`,
+      dieu: curDetail,
+    }
   }
-  if (id.match(/^\d+.\d+$/)) {
+  if (isKhoan(id)) {
     const [dieu, khoan] = id.split('.')
-    return { path: `Khoản ${khoan}, Điều ${dieu}` }
+    return {
+      type: 'khoan',
+      path: `Khoản ${khoan}, Điều ${dieu}`,
+      dieu: curDetail,
+      khoan: getND168ById(khoan),
+    }
   }
-  if (id.match(/^\d+.\d+.(\w|đ)$/)) {
+  if (isDiem(id)) {
     const [dieu, khoan, diem] = id.split('.')
-    return { path: `Điểm ${diem}, khoản ${khoan}, Điều ${dieu}` }
+    return {
+      type: 'diem',
+      path: `Điểm ${diem}, khoản ${khoan}, Điều ${dieu}`,
+      dieu: getND168ById(dieu),
+      khoan: getND168ById(khoan),
+      diem: curDetail,
+    }
   }
+
   return { path: '' }
+}
+
+function isChuong(id: string) {
+  return id.match(/^(I|II|III|IV)$/)
+}
+
+function isMuc(id: string) {
+  return id.match(/^(I|II|III|IV)\.(\d+)$/)
+}
+
+function isDieu(id: string) {
+  return id.match(/^\d+$/)
+}
+
+function isKhoan(id: string) {
+  return id.match(/^\d+.\d+$/)
+}
+
+function isDiem(id: string) {
+  return id.match(/^\d+.\d+.(\w|đ)$/)
 }
