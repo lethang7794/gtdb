16b92aa1eaae63453738ff8b485b838234934f4e
Le Quang Thang <lethang7794@gmail.com>
2025-01-08 18:28:22 +0700

    add more log

--- src/lib/load-file-from-repo.ts
+++ src/lib/load-file-from-repo.ts
@@ -3,9 +3,11 @@ export async function loadFileFromRepo(
   repo = process.env.NEXT_PUBLIC_GITHUB_REPO,
   branch = 'main'
 ) {
+  console.log({ path, repo, branch })
   try {
     const url = `https://raw.githubusercontent.com/${repo}/refs/heads/${branch}/${path}`
     const data = await fetch(url, { cache: 'force-cache' })
+    console.log('🚀 ~ url:', url)
     return data
   } catch (error) {
     throw new Error(`fail loading file from repo: ${error}`)
--- src/service/nghi-dinh-168.ts
+++ src/service/nghi-dinh-168.ts
@@ -3,12 +3,15 @@ import type { ND168 } from '@/model/ND168'
 import { loadFileFromRepo } from '@/lib/load-file-from-repo'
 import { cache } from 'react'
 
-const MARKINGS_REPO_PATH = 'data/nghi-dinh-168.yaml'
+export const MARKINGS_REPO_PATH = 'data/nghi-dinh-168.yaml'
 
 export const getND168s = cache(async () => {
   const resp = await loadFileFromRepo(MARKINGS_REPO_PATH)
+  console.log('🚀 ~ getND168s ~ resp:', resp)
   const body = await resp.text()
+  console.log('🚀 ~ getND168s ~ body:', body)
   const data = yaml.parse(body)
+  console.log('🚀 ~ getND168s ~ data:', data)
   return data as Record<string, ND168>
 })
 
