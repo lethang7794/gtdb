89087c9a48b561470852817388545ed9202ba401
Le Quang Thang <lethang7794@gmail.com>
2025-01-10 02:33:47 +0700

    load nghi dinh 168 mind map from repo

--- src/service/nghi-dinh-168-mind-map.ts
+++ src/service/nghi-dinh-168-mind-map.ts
@@ -1,8 +1,9 @@
+import { cache } from 'react'
 import { loadFileFromRepo } from '@/lib/load-file-from-repo'
 
 const NGHI_DINH_168_MIND_MAP = 'src/content/nghi-dinh-168-mind-map.md'
 
-export const getNghiDinh168MindMap = () => {
+export const getNghiDinh168MindMap = cache(async () => {
   const file = loadFileFromRepo(NGHI_DINH_168_MIND_MAP)
   return file.toString()
-}
+})
