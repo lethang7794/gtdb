7f373c41622fbc57c134c691d751a2cc980d4082
Le Quang Thang <lethang7794@gmail.com>
2025-01-13 02:27:51 +0700

    fix duplicate header in markmaps

--- src/app/markmaps/layout.tsx
+++ src/app/markmaps/layout.tsx
@@ -1,10 +1,8 @@
-import Header from '@/components/block/header'
 import BlankLayout from '@/components/layout/blank-layout'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <BlankLayout>
-      <Header />
       {children}
     </BlankLayout>
   )
