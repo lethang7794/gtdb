df303ebbb328dcd72538c421865d4a4c18767947
Le Quang Thang <lethang7794@gmail.com>
2025-01-13 03:06:14 +0700

    refactor nd168 og

--- src/app/vbpl/nghi-dinh-168-2024/[slug]/opengraph-image.tsx
+++ src/app/vbpl/nghi-dinh-168-2024/[slug]/opengraph-image.tsx
@@ -1,12 +1,9 @@
 import { ImageResponse } from 'next/og'
 
 import { vbplSectionExplain } from '@/lib/vbpl-explain-section'
-import {
-  nd168SectionExplainDetail,
-  nd168SectionExplainComponents,
-} from '@/lib/nd-168-section-explain-detail'
+import { nd168SectionExplainComponents } from '@/lib/nd-168-section-explain-detail'
 import { env } from '@/env.mjs'
-import { Nd168SectionPreview } from './nd-168-section-preview'
+import { LawSectionPreview } from '@/components/block/law-section-preview'
 
 export const size = {
   width: 1200,
@@ -22,7 +19,7 @@ export default async function Image({ params }: { params: { slug: string } }) {
     await nd168SectionExplainComponents(slug)
 
   return new ImageResponse(
-    <Nd168SectionPreview
+    <LawSectionPreview
       short1={short1}
       short2={short2}
       short3={short3}
@@ -30,6 +27,7 @@ export default async function Image({ params }: { params: { slug: string } }) {
       detail2={detail2}
       detail3={detail3}
       highlight={highlight}
+      law="Nghị định 168/2024"
     />,
     {
       width: 1200,
