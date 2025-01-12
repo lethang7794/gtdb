7a0cb5c778c8fb5350faae07491d7c9b91858e3f
Le Quang Thang <lethang7794@gmail.com>
2025-01-13 02:30:28 +0700

    fix nghi dinh 168 path

--- src/service/nghi-dinh-168-mind-map.ts
+++ src/service/nghi-dinh-168-mind-map.ts
@@ -1,7 +1,7 @@
 import { cache } from 'react'
 import { loadFile } from '@/helpers/file-helper'
 
-const NGHI_DINH_168_MIND_MAP = 'data/markmaps/nghi-dinh-168.md'
+const NGHI_DINH_168_MIND_MAP = 'data/markmaps/nghi-dinh-168-2024.md'
 
 export const getNghiDinh168MindMap = cache(async () => {
   const file = await loadFile(NGHI_DINH_168_MIND_MAP)
