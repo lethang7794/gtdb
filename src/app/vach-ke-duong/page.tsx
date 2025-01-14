1dad1b78fecaf21b18a7f3d6a96c7fe3f4c7339a
Le Quang Thang <lethang7794@gmail.com>
2025-01-14 17:18:02 +0700

    add page title for vach ke duong

--- src/app/vach-ke-duong/page.tsx
+++ src/app/vach-ke-duong/page.tsx
@@ -1,5 +1,14 @@
 import Link from 'next/link'
+import type { Metadata } from 'next'
 import { getMarkings, getMarkingImage } from '@/service/marking'
+import { env } from '@/env.mjs'
+import BaseLink from '@/components/base-link'
+
+export const metadata: Metadata = {
+  title: `Vạch kẻ đường | ${env.NEXT_PUBLIC_BRAND_SHORT}`,
+  description:
+    'Tất cả vạch kẻ đường theo QCVN 41:2019/BGTVT và chi tiết từng biển báo',
+}
 
 export default async function Home() {
   const data = await getMarkings()
@@ -8,7 +17,20 @@ export default async function Home() {
 
   return (
     <main className="flex h-full flex-col justify-between p-6 md:p-8">
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(150px,_1fr))] justify-between gap-4">
+      <h1 className="text-center text-4xl font-bold">Vạch kẻ đường</h1>
+
+      <p className="text-center text-2xl text-gray-500">
+        (Bao gồm tất cả vạch kẻ đường theo QCVN 41:2019/BGTVT -{' '}
+        <BaseLink
+          href={'/vbpl/danh-sach'}
+          className="text-blue-500 hover:underline"
+        >
+          Nguồn
+        </BaseLink>
+        )
+      </p>
+
+      <div className="mt-8 grid grid-cols-[repeat(auto-fill,minmax(150px,_1fr))] justify-between gap-4">
         {entries?.map(([key, val]) => {
           const imgUrl = getMarkingImage(val)
 
