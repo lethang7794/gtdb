196dec014ac80b6a3b83c853731e15e354dcf70f
Le Quang Thang <lethang7794@gmail.com>
2025-01-14 17:18:59 +0700

    keep Disclosure in mobile hamburger menu open after clicked

--- src/components/block/header.tsx
+++ src/components/block/header.tsx
@@ -304,14 +304,13 @@ export default function Header() {
                   </DisclosureButton>
                   <DisclosurePanel className="mt-2 space-y-2">
                     {[...products].map((item) => (
-                      <DisclosureButton
+                      <BaseLink
                         key={item.name}
-                        as="a"
                         href={item.href}
                         className="block rounded-lg py-2 pl-6 pr-3 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
                       >
                         {item.name}
-                      </DisclosureButton>
+                      </BaseLink>
                     ))}
                   </DisclosurePanel>
                 </Disclosure>
@@ -326,14 +325,13 @@ export default function Header() {
                   </DisclosureButton>
                   <DisclosurePanel className="mt-2 space-y-2">
                     {[...others].map((item) => (
-                      <DisclosureButton
+                      <BaseLink
                         key={item.name}
-                        as="a"
                         href={item.href}
                         className="block rounded-lg py-2 pl-6 pr-3 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
                       >
                         {item.name}
-                      </DisclosureButton>
+                      </BaseLink>
                     ))}
                   </DisclosurePanel>
                 </Disclosure>
@@ -348,14 +346,13 @@ export default function Header() {
                   </DisclosureButton>
                   <DisclosurePanel className="mt-2 space-y-2">
                     {[...extra].map((item) => (
-                      <DisclosureButton
+                      <BaseLink
                         key={item.name}
-                        as="a"
                         href={item.href}
                         className="block rounded-lg py-2 pl-6 pr-3 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
                       >
                         {item.name}
-                      </DisclosureButton>
+                      </BaseLink>
                     ))}
                   </DisclosurePanel>
                 </Disclosure>
