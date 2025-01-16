ac4ed998997e6e7f5f2e2508229b5b8b21d77797
Le Quang Thang <lethang7794@gmail.com>
2025-01-17 02:05:34 +0700

    refactor markmaps page

---
+++ src/app/markmaps/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Markmap from '@/components/markmap'
+import { processStaticParams } from '@/lib/static-params'
+import { getMarkmapById, getMarkmaps } from '@/service/markmap'
+
+import styles from './slug.module.css'
+import { env } from '@/env.mjs'
+
+type Props = {
+  params: Promise<{ slug: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+export async function generateStaticParams() {
+  const items = await getMarkmaps()
+  const params = Object.keys(items).map((key) => ({ slug: key }))
+  return processStaticParams(params)
+}
+
+export async function generateMetadata({ params }: Props) {
+  const { slug } = await params
+  const item = await getMarkmapById(slug)
+  return {
+    title: `${item.frontmatter.title} | ${env.NEXT_PUBLIC_BRAND_SHORT}`,
+    description: item.frontmatter.abstract,
+  }
+}
+
+async function MarkmapPost({ params }: Props) {
+  const { slug } = await params
+  const item = await getMarkmapById(slug)
+
+  return (
+    <div>
+      <Markmap data={item.content} classNameSvg={styles.markmapSVG} />
+    </div>
+  )
+}
+
+export default MarkmapPost
--- src/service/markmap.ts
+++ src/service/markmap.ts
@@ -2,7 +2,7 @@ import matter from 'gray-matter'
 import { loadFile, readDirectory, readFile } from '@/helpers/file-helper'
 import { MARKMAP_DIR_PATH } from '@/constant/path-repo'
 
-export const getMarkmapList = async () => {
+export const getMarkmaps = async () => {
   const fileNames = await readDirectory(MARKMAP_DIR_PATH)
 
   const markmaps: Record<string, string>[] = []
@@ -21,7 +21,7 @@ export const getMarkmapList = async () => {
   return markmaps.sort((p1, p2) => (p1.publishedOn < p2.publishedOn ? 1 : -1))
 }
 
-export const loadMarkmap = async (slug: string) => {
+export const getMarkmapById = async (slug: string) => {
   const rawContent = await loadFile(`${MARKMAP_DIR_PATH}/${slug}.md`)
   const { data: frontmatter, content } = matter(rawContent)
   return { frontmatter, content: rawContent }
