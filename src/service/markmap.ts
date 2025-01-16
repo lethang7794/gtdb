e51c62a455a3cf4b05719bfa2d4bf13b39d579e2
Le Quang Thang <lethang7794@gmail.com>
2025-01-17 02:12:12 +0700

    read markmap file from source code

--- src/service/markmap.ts
+++ src/service/markmap.ts
@@ -1,14 +1,17 @@
+import path from 'node:path'
 import matter from 'gray-matter'
-import { loadFile, readDirectory, readFile } from '@/helpers/file-helper'
+import { readDirectory, readFile } from '@/helpers/file-helper'
 import { MARKMAP_DIR_PATH } from '@/constant/path-repo'
 
 export const getMarkmaps = async () => {
-  const fileNames = await readDirectory(MARKMAP_DIR_PATH)
+  const dirPath = path.join(process.cwd(), MARKMAP_DIR_PATH)
+  const fileNames = await readDirectory(dirPath)
 
   const markmaps: Record<string, string>[] = []
 
   for (const fileName of fileNames) {
-    const rawContent = await loadFile(`${MARKMAP_DIR_PATH}/${fileName}`)
+    const filePath = path.join(dirPath, fileName)
+    const rawContent = await readFile(filePath)
 
     const { data: frontmatter } = matter(rawContent)
 
@@ -22,7 +25,9 @@ export const getMarkmaps = async () => {
 }
 
 export const getMarkmapById = async (slug: string) => {
-  const rawContent = await loadFile(`${MARKMAP_DIR_PATH}/${slug}.md`)
+  const dirPath = path.join(process.cwd(), MARKMAP_DIR_PATH)
+  const filePath = path.join(dirPath, `${slug}.md`)
+  const rawContent = await readFile(filePath)
   const { data: frontmatter, content } = matter(rawContent)
   return { frontmatter, content: rawContent }
 }
