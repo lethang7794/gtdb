00b18b92a7a882970836a149c39772c3ec747321
Le Quang Thang <lethang7794@gmail.com>
2025-01-17 03:28:46 +0700

    cleanup

--- src/service/markmap.ts
+++ src/service/markmap.ts
@@ -4,18 +4,12 @@ import { readDirectory, readFile } from '@/helpers/file-helper'
 import { MARKMAP_DIR_PATH } from '@/constant/path-repo'
 import { isDev } from '@/env.mjs'
 
-const PWD = isDev
-  ? process.cwd() // Work on local
-  : path.resolve('/vercel/path0') // Work on Vercel
-
 export const getMarkmaps = async () => {
   console.log('process.cwd():', process.cwd())
   console.log('process.env.PWD:', process.env.PWD)
-  // console.log('process.env():', process.env)
   console.log('process.execPath:', process.execPath)
   console.log('process.argv:', process.argv)
   console.log('path.dirname:', path.dirname)
-  console.log('PWD:', PWD)
 
   const dirPath = path.join(MARKMAP_DIR_PATH)
   const fileNames = await readDirectory(dirPath)
@@ -23,7 +17,7 @@ export const getMarkmaps = async () => {
   const markmaps: Record<string, string>[] = []
 
   for (const fileName of fileNames) {
-    const filePath = path.join(dirPath, fileName)
+    const filePath = path.join(MARKMAP_DIR_PATH, fileName)
     const rawContent = await readFile(filePath)
 
     const { data: frontmatter } = matter(rawContent)
@@ -38,8 +32,7 @@ export const getMarkmaps = async () => {
 }
 
 export const getMarkmapById = async (slug: string) => {
-  const dirPath = path.join(MARKMAP_DIR_PATH)
-  const filePath = path.join(dirPath, `${slug}.md`)
+  const filePath = path.join(MARKMAP_DIR_PATH, `${slug}.md`)
   const rawContent = await readFile(filePath)
   const { data: frontmatter, content } = matter(rawContent)
   return { frontmatter, content: rawContent }
