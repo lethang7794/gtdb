0abf9e9f9e1147d1b65ab3794d1be8b60a953453
Le Quang Thang <lethang7794@gmail.com>
2025-01-17 02:05:53 +0700

    import markmap from source code

--- src/service/luat-giao-thong-2024-mind-map.ts
+++ src/service/luat-giao-thong-2024-mind-map.ts
@@ -1,9 +1,7 @@
-import { loadFile } from '@/helpers/file-helper'
 import { unstable_cache } from 'next/cache'
 
-const LUAT_GIAO_THONG_2024_MIND_MAP = 'data/markmaps/luat-TTATGTDB-2024.md'
+import file from '@data/markmaps/luat-TTATGTDB-2024.md'
 
 export const getLuatGT2024MindMap = unstable_cache(async () => {
-  const file = await loadFile(LUAT_GIAO_THONG_2024_MIND_MAP)
   return file
 })
--- src/service/nghi-dinh-168-mind-map.ts
+++ src/service/nghi-dinh-168-mind-map.ts
@@ -1,9 +1,7 @@
-import { loadFile } from '@/helpers/file-helper'
 import { unstable_cache } from 'next/cache'
 
-const NGHI_DINH_168_MIND_MAP = 'data/markmaps/nghi-dinh-168-2024.md'
+import file from '@data/markmaps/nghi-dinh-168-2024.md'
 
 export const getNghiDinh168MindMap = unstable_cache(async () => {
-  const file = await loadFile(NGHI_DINH_168_MIND_MAP)
   return file
 })
