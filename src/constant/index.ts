7dab642e6681410ee50b716c5f2bae7f86a15648
Le Quang Thang <lethang7794@gmail.com>
2025-01-17 16:25:37 +0700

    add cacheWithRevalidate

---
+++ src/constant/index.ts
@@ -0,0 +1,9 @@
+import { CACHE_REVALIDATION_DEFAULT } from './cache'
+import { LAW_ABBR } from './laws'
+
+export const constants = {
+  cache: {
+    CACHE_REVALIDATION_DEFAULT,
+  },
+  laws: LAW_ABBR,
+} as const
---
+++ src/lib/cache.ts
@@ -0,0 +1,23 @@
+import { env } from '@/env.mjs'
+import { unstable_cache } from 'next/cache'
+
+type Callback = <I>(...args: I[]) => Promise<I>
+
+export function cacheWithRevalidate<T extends Callback>(
+  cb: T,
+  keyParts?: string[],
+  options?: {
+    /**
+     * The revalidation interval in seconds.
+     */
+    revalidate?: number | false
+    tags?: string[]
+  }
+): T {
+  const cached = unstable_cache(cb, keyParts, {
+    revalidate: env.NEXT_PUBLIC_CACHE_REVALIDATION,
+    tags: keyParts,
+  })
+
+  return cached
+}
