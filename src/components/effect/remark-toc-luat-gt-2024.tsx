249c6cfeab8e16816ff70d68a4df822c52d10ea5
Lê Quang Thắng <lethang7794@gmail.com>
2025-01-19 00:55:18 +0700

    remove loading in vbpl (#19)

--- src/components/effect/remark-toc-luat-gt-2024.tsx
+++ src/components/effect/remark-toc-luat-gt-2024.tsx
@@ -1,18 +1,25 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useLayoutEffect } from 'react'
 import { useRouter } from 'next/navigation'
 
 export default function RemarkTOCEffect() {
   const router = useRouter()
 
-  useEffect(() => {
-    // Disable scroll
-    const body = document.body
-    body.style.overflow = 'hidden'
+  useLayoutEffect(() => {
     // Add background for body
+    const body = document.body
     body.classList.add('body-bg-markdown')
 
+    // Go to fragment
+    router.replace(window.location.href)
+
+    return () => {
+      body.classList.remove('body-bg-markdown')
+    }
+  }, [router])
+
+  useEffect(() => {
     // Cleanup TOC
     const tocItems = document.querySelectorAll(
       '#mục-lục + ul li > a .anchor-link'
@@ -30,23 +37,11 @@ export default function RemarkTOCEffect() {
       }
     }
 
-    // Go to fragment
-    router.replace(window.location.href)
-
-    setTimeout(() => {
-      // Remove spinner
-      const spinner = document.getElementById('spinner')
-      if (spinner) {
-        spinner.style.display = 'none'
-      }
-
-      // Enable scroll
-      body.style.overflow = 'initial'
-    }, 0)
-
-    return () => {
-      body.classList.remove('body-bg-markdown')
+    // Show TOC after go to id
+    const liItems = document.querySelectorAll('h2#mục-lục + ul > li')
+    for (const item of Array.from(liItems)) {
+      item.classList.add('display-revert')
     }
-  }, [router])
+  }, [])
   return null
 }
--- src/components/effect/remark-toc.tsx
+++ src/components/effect/remark-toc.tsx
@@ -1,18 +1,25 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useLayoutEffect } from 'react'
 import { useRouter } from 'next/navigation'
 
 export default function RemarkTOCEffect() {
   const router = useRouter()
 
-  useEffect(() => {
-    // Disable scroll
-    const body = document.body
-    body.style.overflow = 'hidden'
+  useLayoutEffect(() => {
     // Add background for body
+    const body = document.body
     body.classList.add('body-bg-markdown')
 
+    // Go to fragment
+    router.replace(window.location.href)
+
+    return () => {
+      body.classList.remove('body-bg-markdown')
+    }
+  }, [router])
+
+  useEffect(() => {
     // Cleanup TOC
     const tocItems = document.querySelectorAll('#mục-lục + ul li > a > span')
     for (const item of Array.from(tocItems)) {
@@ -47,40 +54,11 @@ export default function RemarkTOCEffect() {
       }
     }
 
-    // Go to fragment
-    router.replace(window.location.href)
-
-    // const href = window.location.href
-    // const hash = window.location.hash
-    // const parts = hash?.split('.') || []
-    // if (parts.length > 1) {
-    //   parts.pop()
-    // }
-    // const href =
-    //   window.location.origin + window.location.pathname + parts.join('.')
-    // console.log({ hash, parts, href, 'window.location': window.location })
-    // const hash = window.location.hash
-    // const id = hash.replace('#', '')
-    // if (!id) {
-    //   return
-    // }
-    // const el = document.getElementById(id)
-    // el?.scrollIntoView({ behavior: 'instant' })
-
-    setTimeout(() => {
-      // Remove spinner
-      const spinner = document.getElementById('spinner')
-      if (spinner) {
-        spinner.style.display = 'none'
-      }
-
-      // Enable scroll
-      body.style.overflow = 'initial'
-    }, 0)
-
-    return () => {
-      body.classList.remove('body-bg-markdown')
+    // Show TOC after go to id
+    const liItems = document.querySelectorAll('h2#mục-lục + ul > li')
+    for (const item of Array.from(liItems)) {
+      item.classList.add('display-revert')
     }
-  }, [router])
+  }, [])
   return null
 }
