f17467d5531ddfc85f97d978f555bf9065a98de9
Le Quang Thang <lethang7794@gmail.com>
2025-01-21 03:24:26 +0700

    fix footer

--- src/components/block/footer.tsx
+++ src/components/block/footer.tsx
@@ -3,7 +3,7 @@ import BaseLink from '@/components/base-link'
 
 export function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 border-t">
+    <footer className="bg-white shadow dark:bg-gray-900 border-t">
       <div className="w-full max-w-screen-xl mx-auto p-4 px-6 lg:px-8">
         <div className="flex items-center justify-between">
           <BaseLink
