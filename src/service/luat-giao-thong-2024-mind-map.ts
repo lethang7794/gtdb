4937a717979ca8661d5be2ff54c00c843b3f4933
Le Quang Thang <lethang7794@gmail.com>
2025-01-22 23:12:31 +0700

    cleanup

--- src/service/luat-giao-thong-2024-mind-map.ts
+++
@@ -1,6 +0,0 @@
-const file = '@data/markmaps/luat-TTATGTDB-2024.md'
-
-export const getLuatGT2024MindMap = async () => {
-  console.log('🚀 ~ getLuatGT2024MindMap ~ file:', file)
-  return file
-}
--- src/service/nghi-dinh-168-mind-map.ts
+++
@@ -1,6 +0,0 @@
-const file = '@/data/markmaps/nghi-dinh-168-2024.md'
-
-export const getNghiDinh168MindMap = async () => {
-  console.log('🚀 ~ file:', file)
-  return file
-}
