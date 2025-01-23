e54d0991fdce8fcbe15e932e15baacbdf67d407f
Le Quang Thang <lethang7794@gmail.com>
2025-01-23 23:52:45 +0700

    remove VERCEL_ENV

--- src/lib/static-params.ts
+++ src/lib/static-params.ts
@@ -1,8 +1,11 @@
 import { env } from '@/env.mjs'
 
 export function processStaticParams(params: { slug: string }[]) {
-  if (env.VERCEL_ENV === 'production') {
-    return params
+  if (env.NEXT_PUBLIC_APP_ENV === 'dev') {
+    return params.slice(0, 5)
   }
-  return params.slice(0, 5)
+  if (env.NEXT_PUBLIC_APP_ENV === 'preview') {
+    return params.slice(0, 50)
+  }
+  return params
 }
