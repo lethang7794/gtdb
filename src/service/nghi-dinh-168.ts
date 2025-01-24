8828163a2dc74a75a4c1d014d056f7434edb71dd
Lê Quang Thắng <lethang7794@gmail.com>
2025-01-24 11:42:57 +0700

    remove unstable_cache (#50)

--- src/service/nghi-dinh-168.ts
+++ src/service/nghi-dinh-168.ts
@@ -1,14 +1,11 @@
 import type { ND168 } from '@/model/ND168'
-import { unstable_cache } from 'next/cache'
 import data from '@data/nghi-dinh-168.yaml'
 
-export const getND168s = unstable_cache(async () => {
+export const getND168s = async () => {
   return data as Record<string, ND168>
-})
+}
 
-export const getND168ById = unstable_cache(
-  async (id: string): Promise<ND168 | undefined> => {
-    const items = await getND168s()
-    return items[id]
-  }
-)
+export const getND168ById = async (id: string): Promise<ND168 | undefined> => {
+  const items = await getND168s()
+  return items[id]
+}
