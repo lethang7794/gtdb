ad898b2893554ff6b1ce26ae33d304cb32640c4f
Le Quang Thang <lethang7794@gmail.com>
2025-01-24 11:31:07 +0700

    remove unstable_cache

--- src/service/luat-giao-thong-2024.ts
+++ src/service/luat-giao-thong-2024.ts
@@ -1,14 +1,13 @@
 import type { LuatGT2024 } from '@/model/LuatGT2024'
-import { unstable_cache } from 'next/cache'
 import data from '@data/luat-TTATGTDT-2024.yaml'
 
-export const getLuatGT2024s = unstable_cache(async () => {
+export const getLuatGT2024s = async () => {
   return data as Record<string, LuatGT2024>
-})
+}
 
-export const getLuatGT2024ById = unstable_cache(
-  async (id: string): Promise<LuatGT2024 | undefined> => {
-    const items = await getLuatGT2024s()
-    return items[id]
-  }
-)
+export const getLuatGT2024ById = async (
+  id: string
+): Promise<LuatGT2024 | undefined> => {
+  const items = await getLuatGT2024s()
+  return items[id]
+}
