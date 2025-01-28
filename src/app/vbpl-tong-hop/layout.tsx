91ed87f92878a41aa7a5e52f8f7ba419fb96563f
Le Quang Thang <lethang7794@gmail.com>
2025-01-28 16:54:42 +0700

    refactor pages

---
+++ src/app/vbpl-tong-hop/layout.tsx
@@ -0,0 +1,14 @@
+import 'github-markdown-css/github-markdown.css'
+import '@/style/github-markdown.css'
+import '@/style/github-markdown-custom.css'
+import { GITHUB_MARKDOWN_CSS_CLASS } from '@/constant/github-markdown-css'
+
+export default function MdxLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={`${GITHUB_MARKDOWN_CSS_CLASS} font-sans`}>
+      <div className="relative flex h-full flex-col justify-between py-6 md:py-8">
+        <div className="container pr-4 pl-8">{children}</div>
+      </div>
+    </div>
+  )
+}
