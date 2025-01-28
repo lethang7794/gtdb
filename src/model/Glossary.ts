3a52fb04bfcd301544df9aaae280f27a94ee2e34
Le Quang Thang <lethang7794@gmail.com>
2025-01-28 16:53:29 +0700

    reformat glossary

--- src/model/Glossary.ts
+++ src/model/Glossary.ts
@@ -1,4 +1,5 @@
 export type Glossary = {
+  id: string
   source: string
   source_abbr: string
   docs: string
--- src/service/glossary.ts
+++ src/service/glossary.ts
@@ -1,14 +1,13 @@
 import type { Glossary } from '@/model/Glossary'
-import { unstable_cache } from 'next/cache'
-import data from '@data/glossaries.yaml'
+import file from '@data/glossaries.yaml'
 
-export const getGlossaries = unstable_cache(async () => {
-  return data as Record<string, Partial<Glossary>>
-})
+export const getGlossaries = async () => {
+  return (file.data || []) as Glossary[]
+}
 
-export const getGlossaryById = unstable_cache(
-  async (id: string): Promise<Partial<Glossary> | undefined> => {
-    const items = await getGlossaries()
-    return items[id]
-  }
-)
+export const getGlossaryById = async (
+  id: string
+): Promise<Partial<Glossary>> => {
+  const items = await getGlossaries()
+  return items.filter((item) => item.id)[0]
+}
