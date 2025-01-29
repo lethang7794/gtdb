7b2483215d01da121310f9fcb1511627a63646bf
Le Quang Thang <lethang7794@gmail.com>
2025-01-29 17:28:55 +0700

    reformat layout button

--- src/app/bbgt/list-options.tsx
+++ src/app/bbgt/list-options.tsx
@@ -7,6 +7,8 @@ import {
   useSearchParams,
 } from 'next/navigation'
 import React, { useLayoutEffect } from 'react'
+import { GalleryVerticalEnd, Grid2x2, Grid3x3 } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 const DEFAULT_LAYOUT = 'more-cols'
 
@@ -66,27 +68,43 @@ export function ListOptions({ children }: ListOptionsProps) {
 
   return (
     <>
-      Current layout: {layout}
-      <div
-        onClick={(e) => {
-          handleChangeLayoutState(searchParams, 'one-col')
-        }}
-      >
-        One
-      </div>
-      <div
-        onClick={(e) => {
-          handleChangeLayoutState(searchParams, 'less-cols')
-        }}
-      >
-        Small
-      </div>
-      <div
-        onClick={(e) => {
-          handleChangeLayoutState(searchParams, 'more-cols')
-        }}
-      >
-        Big
+      <div className="mt-4 flex items-center">
+        Bố cục:
+        <div
+          onClick={(e) => {
+            handleChangeLayoutState(searchParams, 'one-col')
+          }}
+          className={cn(
+            'ml-2 flex gap-1 border p-3',
+            layout === 'one-col' && 'bg-gray-500 text-white'
+          )}
+        >
+          <GalleryVerticalEnd size="24" />1 cột
+        </div>
+        <div
+          onClick={(e) => {
+            handleChangeLayoutState(searchParams, 'less-cols')
+          }}
+          className={cn(
+            'flex gap-1 border border-l-0 p-3',
+            layout === 'less-cols' && 'bg-gray-500 text-white'
+          )}
+        >
+          <Grid2x2 size="24" />
+          Ít cột
+        </div>
+        <div
+          onClick={(e) => {
+            handleChangeLayoutState(searchParams, 'more-cols')
+          }}
+          className={cn(
+            'flex gap-1 border border-l-0 p-3',
+            layout === 'more-cols' && 'bg-gray-500 text-white'
+          )}
+        >
+          <Grid3x3 size="24" />
+          Nhiều cột
+        </div>
       </div>
       {children}
     </>
