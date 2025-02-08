e11e41f83c6e67b91492e4f26fcbce3fcd8eeced
Le Quang Thang <lethang7794@gmail.com>
2025-02-09 04:39:56 +0700

    fix sitemap url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_DOMAIN,
+  siteUrl: `https://${process.env.NEXT_PUBLIC_DOMAIN}`,
   generateRobotsTxt: true, // (optional)
   // ...other options
-  output: "export"
+  output: 'export',
 }
