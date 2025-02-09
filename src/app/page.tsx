e721e1159cdd1ee17de47fa119c951967033e1af
Lê Quang Thắng <lethang7794@gmail.com>
2025-02-09 20:38:45 +0700

    update home layout (#93)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,12 +20,12 @@ export default async function Home() {
       <div className="flex h-full flex-col justify-start bg-[linear-gradient(to_bottom,#dbf4ff,#fff1f1)]">
         <div className="container px-4 pb-8 md:px-8">
           <div className="grid gap-4">
-            <div className="mt-4 grid grid-cols-2 gap-4 rounded-lg text-center font-mono text-sm leading-6 font-bold text-white sm:grid-cols-2">
+            <div className="mt-4 grid grid-cols-2 gap-4 rounded-lg text-center font-mono text-base leading-6 font-bold text-white sm:grid-cols-2">
               <BaseLink
                 href={PATHS.vbpl.NGHI_DINH_168}
                 className="row-span-4 grid place-items-center gap-4 rounded-lg bg-red-600 p-4"
               >
-                <Receipt className="h-12 w-12 self-end" />
+                <Receipt className="h-20 w-20 self-end" />
                 <div className="self-start">
                   MỨC PHẠT
                   <div className="text-sm italic">(Nghị định 168/2024)</div>
@@ -33,35 +33,35 @@ export default async function Home() {
               </BaseLink>
               <BaseLink
                 href={PATHS.vbpl.NGHI_DINH_168}
-                className="col-span-1 grid place-content-center rounded-lg bg-red-400 p-4 text-white dark:bg-red-800 dark:text-red-400"
+                className="col-span-1 grid place-content-center rounded-lg border-4 border-red-600 bg-transparent text-black p-4"
               >
                 Toàn văn 🗞️
               </BaseLink>
               <BaseLink
                 href={PATHS.markmaps.NGHI_DINH_168_2024}
-                className="col-span-1 grid place-content-center rounded-lg bg-red-400 p-4 text-white dark:bg-red-800 dark:text-red-400"
+                className="col-span-1 grid place-content-center rounded-lg border-4 border-red-600 bg-transparent text-black p-4"
               >
                 Tóm tắt
               </BaseLink>
               <BaseLink
                 href={PATHS.markmaps.NGHI_DINH_168_2024_XE_OTO}
-                className="col-span-1 grid place-content-center rounded-lg bg-red-400 p-4 text-white dark:bg-red-800 dark:text-red-400"
+                className="col-span-1 grid place-content-center rounded-lg border-4 border-red-600 bg-transparent text-black p-4"
               >
                 Xe ôtô 🚘
               </BaseLink>
               <BaseLink
                 href={PATHS.markmaps.NGHI_DINH_168_2024_XE_MOTO}
-                className="col-span-1 grid place-content-center rounded-lg bg-red-400 p-4 text-white dark:bg-red-800 dark:text-red-400"
+                className="col-span-1 grid place-content-center rounded-lg border-4 border-red-600 bg-transparent text-black p-4"
               >
                 Xe môtô 🏍️
               </BaseLink>
             </div>
-            <div className="grid grid-cols-2 gap-4 rounded-lg text-center font-mono text-sm leading-6 font-bold text-white sm:grid-cols-2">
+            <div className="grid grid-cols-2 gap-4 rounded-lg text-center font-mono text-base leading-6 font-bold text-white sm:grid-cols-2">
               <BaseLink
                 href={PATHS.vbpl.LUAT_GT_2024}
-                className="row-span-2 grid place-items-center gap-4 rounded-lg bg-yellow-400 p-4 text-yellow-800"
+                className="row-span-2 grid place-items-center gap-4 rounded-lg bg-yellow-300 p-4 text-black"
               >
-                <Scale className="h-12 w-12 self-end" />
+                <Scale className="h-20 w-20 self-end" />
                 <div>
                   LUẬT
                   <div className="text-sm italic">(Luật TTATGTĐB 2024)</div>
@@ -69,49 +69,49 @@ export default async function Home() {
               </BaseLink>
               <BaseLink
                 href={PATHS.vbpl.LUAT_GT_2024}
-                className="col-span-1 grid place-content-center rounded-lg bg-yellow-300 p-4 text-yellow-800 dark:bg-yellow-800 dark:text-yellow-400"
+                className="col-span-1 grid place-content-center rounded-lg p-4 border-4 border-yellow-300 bg-transparent text-black"
               >
                 Toàn văn 🗞️
               </BaseLink>
               <BaseLink
                 href={PATHS.markmaps.LUAT_TTATGTDB_2024}
-                className="col-span-1 grid place-content-center rounded-lg bg-yellow-300 p-4 text-yellow-800 dark:bg-yellow-800 dark:text-yellow-400"
+                className="col-span-1 grid place-content-center rounded-lg p-4 border-4 border-yellow-300 bg-transparent text-black"
               >
                 Tóm tắt
               </BaseLink>
             </div>
-            <div className="grid grid-cols-2 gap-4 rounded-lg text-center font-mono text-sm leading-6 font-bold text-white sm:grid-cols-2">
+            <div className="grid grid-cols-2 gap-4 rounded-lg text-center font-mono text-base leading-6 font-bold text-white sm:grid-cols-2">
               <BaseLink
                 href={PATHS.bbgt.ROOT}
-                className="row-span-2 grid place-items-center justify-center gap-4 rounded-lg bg-green-500 p-4 text-green-800"
+                className="row-span-2 grid place-items-center justify-center gap-4 rounded-lg bg-green-400 p-4 text-black"
               >
-                <Signpost className="h-12 w-12 self-end" />
+                <Signpost className="h-20 w-20 self-end" />
                 <div className="self-start">BIỂN BÁO</div>
               </BaseLink>
               <BaseLink
                 href={PATHS.bbgt.ROOT}
-                className="col-span-1 grid place-content-center rounded-lg bg-green-300 p-4 text-green-800 dark:bg-green-800 dark:text-green-400"
+                className="col-span-1 grid place-content-center rounded-lg p-4 border-4 border-green-400 bg-transparent text-black"
               >
                 Danh sách
               </BaseLink>
               <BaseLink
                 href={PATHS.bbgtSoSanh.ROOT}
-                className="col-span-1 grid place-content-center rounded-lg bg-green-300 p-4 text-green-800 dark:bg-green-800 dark:text-green-400"
+                className="col-span-1 grid place-content-center rounded-lg p-4 border-4 border-green-400 bg-transparent text-black"
               >
                 So sánh
               </BaseLink>
             </div>
-            <div className="grid grid-cols-2 gap-4 rounded-lg text-center font-mono text-sm leading-6 font-bold text-white sm:grid-cols-2">
+            <div className="grid grid-cols-2 gap-4 rounded-lg text-center font-mono text-base leading-6 font-bold text-white sm:grid-cols-2">
               <BaseLink
                 href={PATHS.vachKeDuong.ROOT}
                 className="row-span-4 grid place-items-center justify-center gap-4 rounded-lg bg-blue-500 p-4"
               >
-                <Paintbrush className="h-12 w-12" />
+                <Paintbrush className="h-20 w-20" />
                 <div>VẠCH KẺ ĐƯỜNG</div>
               </BaseLink>
               <BaseLink
                 href={PATHS.vachKeDuong.ROOT}
-                className="row-span-4 grid place-content-center rounded-lg bg-blue-300 p-4 text-white dark:bg-blue-800 dark:text-blue-400"
+                className="row-span-4 grid place-content-center rounded-lg p-4 border-4 border-blue-500 bg-transparent text-black"
               >
                 Danh sách
               </BaseLink>
@@ -125,7 +125,7 @@ export default async function Home() {
                 <li key={item.name + item.url}>
                   <div className="flex gap-2">
                     <BaseLink
-                      className="mr-2 py-2 text-blue-600 hover:underline dark:text-blue-500"
+                      className="mr-2 py-2 text-gray-800 hover:underline"
                       href={item.url}
                     >
                       {item.name}
@@ -149,7 +149,7 @@ export default async function Home() {
                 <li key={item.name + item.url}>
                   <div className="flex gap-2">
                     <BaseLink
-                      className="mr-2 py-2 text-blue-600 hover:underline dark:text-blue-500"
+                      className="mr-2 py-2 text-gray-800 hover:underline"
                       href={item.url}
                     >
                       {item.name}
@@ -173,7 +173,7 @@ export default async function Home() {
                 <li key={item.name + item.url}>
                   <div className="flex gap-2">
                     <BaseLink
-                      className="mr-2 py-2 text-blue-600 hover:underline dark:text-blue-500"
+                      className="mr-2 py-2 text-gray-800 hover:underline"
                       href={item.url}
                     >
                       {item.name}
